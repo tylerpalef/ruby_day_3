7ae07d7927baf28fcb2b25fd08092522a206c9f4
“Tyler <tylerpalef@gmail.com>
2018-07-18 17:38:43 -0400

    exercise 0 done

---
+++ exercise1.rb
@@ -0,0 +1,37 @@
+# Making a bunch of arrays for exercise 0
+
+fav_colours = ["red", "green", "blue", "yellow", "pink", "black"]
+
+age = [24, 22, 26, 3]
+
+coin_flip = ["heads", "heads", "heads","tails", "tails"]
+
+performing_artists = ["The Killers", "Sum41", "Ninja Sex Party"]
+
+fav_colours_symbols = [:red, :green, :blue, :yellow, :pink, :black]
+
+#Hash
+
+dictionary = {
+  :hello => "this is a greeting",
+  :poop => "you don't want to know",
+  :sweet => "something nice"
+}
+
+movies = {
+  :Spiderman => 2002,
+  :Frozen => 2012,
+  :Paul_Blart_Mall_Cop => 2014
+}
+
+population = {
+  :Toronto => 5,
+  :Vancouver => 3,
+  :Montreal => 1
+}
+
+sibling = {
+  :Jacqui => 26,
+  :Billy => 29,
+  :Tyler => 24
+}
